2a8ca70b3263cb46a56d9c9b7d5412d6f73ced95
aynur <aynur.kaysoy@gmail.com>
2021-09-18 09:08:18 +0200

    front4

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -116,7 +116,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('verify-success', 'Email vérifié ! Vous pouvez vous connecter =)');
 
         if($this->isGranted('ROLE_ADMIN'))
         {
